eda5aae2e35904d23a0bb6e07f120742f4d59488
Christopher Beddow <christopher@mapillary.com>
2017-05-02 19:46:25 +0400

    Add files via upload

---
+++ setting/nls/strings.js
@@ -0,0 +1,9 @@
+/* globals define */
+define({
+  root: ({
+    defaultCoverage: 'View default coverage:',
+    clientId: 'Mapillary clientID',
+    obtainClientId: 'After signing up at mapillary.com, the clientID can be obtained at',
+    instructions: 'Please paste it in the clientID field.'
+  })
+})
